0d7310e87750539db7535927a01ae6d9f548fe9d
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-10-09 18:32:07 +0100

    Update ApplicationSummary.jsx

--- src/pages/dashboard/ApplicationSummary.jsx
+++ src/pages/dashboard/ApplicationSummary.jsx
@@ -96,7 +96,8 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState, useContext } from 'react';
 import firebaseExports from '../../utils/firebase';
 import { doc, getDoc } from 'firebase/firestore';
-import { AuthContext } from '../../context/AuthContext'; // Ensure correct import
+import AuthContext from '../../context/AuthContext'; // Default import
+import { useContext } from 'react';
 import styles from './styles/applicationsummary.module.css';
 
 const ApplicationSummary = () => {
